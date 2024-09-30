fd9f26e80f5ec1f34201b6873cbc3d7804c19619
steve234qwe <130405249+steve234qwe@users.noreply.github.com>
2024-09-29 20:22:57 -0600

    Mapa Agregado

--- TouristRoute/TouristRoute.cpp
+++ TouristRoute/TouristRoute.cpp
@@ -1,27 +1,84 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+void mapMenu(Texture & mapTexture)
+{
+	RenderWindow windowMap(VideoMode(1280, 720), "Mapa Tour");
+	Sprite spriteMap;
+	spriteMap.setTexture(mapTexture);
+	while (windowMap.isOpen())
+	{
+		Event eventMap{};
+		while (windowMap.pollEvent(eventMap))
+		{
+			if (eventMap.type == Event::MouseButtonPressed)
+			{
+				int xMap = eventMap.mouseButton.x;
+				int yMap = eventMap.mouseButton.y;
+				if (xMap >= 1201 && xMap <= 1251 && yMap >= 611 && yMap <= 677)
+				{
+					windowMap.close();
+				}
+			}
+		}
+		windowMap.clear();
+		windowMap.draw(spriteMap);
+		windowMap.display();
+	}
+}
+
+void clickMenu(Event& event,Texture& mapTexture, RenderWindow& window)
+{
+	if (event.type == Event::MouseButtonPressed)
+	{
+		int x = event.mouseButton.x;
+		int y = event.mouseButton.y;
+
+		if (x >= 54 && x <= 232 && y >= 598 && y <= 657)
+		{
+			mapMenu(mapTexture);
+		}
+		if (x >= 1201 && x <= 1251 && y >= 611 && y <= 677)
+		{
+			window.close();
+		}
+	}
+}
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
+	RenderWindow window(VideoMode(1280, 720), "SFML works!");
 
-    sf::Texture texture;
-    if (!texture.loadFromFile("FondoInicial.png")) {
-        return -1; 
-    }
+	Texture texture;
+	if (!texture.loadFromFile("FondoInicial.png"))
+	{
+		return -1;
+	}
 
-    sf::Sprite sprite;
-    sprite.setTexture(texture);
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-            window.close();
-        }
-        window.clear();
-        window.draw(sprite); 
-        window.display();
-    }
-    return 0;
+	Texture mapTexture;
+	if (!mapTexture.loadFromFile("MapaBase.png"))
+	{
+		return -1;
+	}
+
+	Sprite sprite;
+	sprite.setTexture(texture);
+
+	while (window.isOpen())
+	{
+		Event event{};
+		while (window.pollEvent(event))
+		{
+
+			clickMenu(event, mapTexture, window);
+		}
+		window.clear();
+		window.draw(sprite);
+		window.display();
+	}
+	return 0;
 }
